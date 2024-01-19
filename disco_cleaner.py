4c88f896c3390130092ffbe8ba12874016295902
Auto <37911007+autonomoid@users.noreply.github.com>
2024-01-19 22:46:13 +0000

    Update disco_cleaner.py
    Now supports multiple servers

--- disco_cleaner.py
+++ disco_cleaner.py
@@ -1,10 +1,17 @@
-import discord
-from datetime import datetime, timedelta
+API_TOKEN = '<your_token>'
 
-API_TOKEN = 'ABCDEFGHIJKLMNOP'
-SERVER_ID = 123456789
-RETENTION_PERIOD = 90 # days
-BOT_CHANNEL_ID = 987654321
+servers = {
+    '<server 1>': {
+        'server_id': <server id>,
+        'bot_channel_id': <chan id>,
+        'retention_period': 60
+    },
+    '<server 2>': {
+        'server_id': <server id>,
+        'bot_channel_id': <chan id>,
+        'retention_period': 60
+    }
+}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,27 +26,33 @@ async def delete_old_messages(messages):
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-    server = client.get_guild(SERVER_ID)
-    if not server:
-        print(f"Server with ID {SERVER_ID} not found.")
-        return
-    else:
-        print(f"Cleaning server '{server.name}'.") 
+    for server_name in servers:
 
-    bot_channel = server.get_channel(BOT_CHANNEL_ID)
-    await bot_channel.send(f"Cleanup started. Retention period is {RETENTION_PERIOD} days.")
+        SERVER_ID = servers[server_name]['server_id']
+        BOT_CHANNEL_ID = servers[server_name]['bot_channel_id']
+        RETENTION_PERIOD = servers[server_name]['retention_period']
 
-    now = datetime.utcnow()
-    cutoff_date = now - timedelta(days=RETENTION_PERIOD)
+        server = client.get_guild(SERVER_ID)
+        if not server:
+            print(f"Server with ID {SERVER_ID} not found.")
+            return
+        else:
+            print(f"Cleaning server '{server.name}'.") 
 
-    text_channels = [channel for channel in server.channels if str(channel.type) == 'text']
-    for channel in text_channels:
-        print(f"Found channel: '{channel.name}'.")
-        old_messages = [message async for message in channel.history(limit=None, before=cutoff_date)]
-        message_count = len(old_messages)
-        if message_count > 0:
-            await delete_old_messages(old_messages)
-            await bot_channel.send(f"{message_count} messages deleted from channel '{channel.name}'.")
+        bot_channel = server.get_channel(BOT_CHANNEL_ID)
+        await bot_channel.send(f"Cleanup started. Retention period is {RETENTION_PERIOD} days.")
+
+        now = datetime.utcnow()
+        cutoff_date = now - timedelta(days=RETENTION_PERIOD)
+
+        text_channels = [channel for channel in server.channels if str(channel.type) == 'text']
+        for channel in text_channels:
+            print(f"Found channel: '{channel.name}'.")
+            old_messages = [message async for message in channel.history(limit=None, before=cutoff_date)]
+            message_count = len(old_messages)
+            if message_count > 0:
+                await delete_old_messages(old_messages)
+                await bot_channel.send(f"{message_count} messages deleted from channel '{channel.name}'.")
     
     await bot_channel.send(f"Cleanup completed.")
     await client.close()
